b9325cedd53f8e1d2839ea61a911bb010da2259f
axel.bois <axel.bois@iut-rodez.fr>
2023-10-03 16:17:58 +0200

    Test

---
+++ src/main/java/but3/Aboi/App.java
@@ -0,0 +1,13 @@
+package but3.Aboi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/but3/Aboi/AppTest.java
@@ -0,0 +1,15 @@
+package but3.Aboi;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
