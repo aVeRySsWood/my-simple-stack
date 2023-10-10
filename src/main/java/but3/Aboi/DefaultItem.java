c4baf53bb3a1bab43221ac8b22b0c4cf05d331f3
aVeRySsWood <90342894+aVeRySsWood@users.noreply.github.com>
2023-10-10 16:25:37 +0200

    Ajout d'un autre commentaire

--- src/main/java/but3/Aboi/DefaultItem.java
+++ src/main/java/but3/Aboi/DefaultItem.java
@@ -5,6 +5,7 @@ package but.info3.fsil;
 
   // C'est vraiment l'item Default ???
   //WOW mais c'est une dinguerie chacal !!!!!
+  //Simon tu deheks
  
 public class DefaultItem implements Item {
 }
