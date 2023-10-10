9862947ff1e819799fc0600f4344181dddc30ab6
aVeRySsWood <90342894+aVeRySsWood@users.noreply.github.com>
2023-10-10 16:24:36 +0200

    Ajout d'un commentaire
    Création d'un commentaire de la class DafaultItem.

--- src/main/java/but3/Aboi/DefaultItem.java
+++ src/main/java/but3/Aboi/DefaultItem.java
@@ -4,6 +4,7 @@ package but.info3.fsil;
   Class describing a dummy Item.
 
   // C'est vraiment l'item Default ???
+  //WOW mais c'est une dinguerie chacal !!!!!
  
 public class DefaultItem implements Item {
 }
